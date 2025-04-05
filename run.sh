38a5d2724bf6662af9b738a89cc889bfeae079ac
dennism-tulcolabs <dennis.michalopoulos@tulcolabs.com>
2025-04-05 11:33:08 -0500

    Rename service/app.py -> api.py

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if [[ train == $COMMAND ]]; then
       --model-name "catboost" \
       --model-name "lgbm"
 elif [[ serve == $COMMAND ]]; then
-    docker run -v usf-model-api-root:/package -p 80:80 usf-model-api:latest pipenv run fastapi run ./service/app.py --port 80
+    docker run -v usf-model-api-root:/package -p 80:80 usf-model-api:latest pipenv run fastapi run ./service/api.py --port 80
 elif [[ pytest == $COMMAND ]]; then
     docker run -v usf-model-api-root:/package usf-model-api:latest pipenv run pytest ./tests ./service/routers/sales_forecasting/test_router.py
 else
