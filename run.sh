4e6eb20e00b680c7b2633c26e16e345b249fc88e
dennism-tulcolabs <dennis.michalopoulos@tulcolabs.com>
2025-04-05 13:18:03 -0500

    Add more

--- run.sh
+++ run.sh
@@ -4,14 +4,28 @@ set -ex
 
 COMMAND="$1"
 
-if [[ train == $COMMAND ]]; then
-    docker run -v usf-model-api-root:/package usf-model-api:latest pipenv run python ./models/sales_forecasting/train.py \
+run_train() {
+  docker run -v usf-model-api-root:/package usf-model-api:latest pipenv run python ./models/sales_forecasting/train.py \
       --model-name "catboost" \
       --model-name "lgbm"
+}
+
+run_serve() {
+  docker run -v usf-model-api-root:/package -p 80:80 usf-model-api:latest pipenv run fastapi run ./service/api.py --port 80
+}
+
+run_pytest() {
+  docker run -v usf-model-api-root:/package usf-model-api:latest pipenv run pytest ./tests ./service/routers/sales_forecasting/test_router.py
+}
+
+if [[ train == $COMMAND ]]; then
+    run_train
 elif [[ serve == $COMMAND ]]; then
-    docker run -v usf-model-api-root:/package -p 80:80 usf-model-api:latest pipenv run fastapi run ./service/api.py --port 80
+    run_serve
+elif [[ launch == $COMMAND ]]; then
+    run_train && wait && run_serve
 elif [[ pytest == $COMMAND ]]; then
     docker run -v usf-model-api-root:/package usf-model-api:latest pipenv run pytest ./tests ./service/routers/sales_forecasting/test_router.py
 else
-    echo "No command provided. Available commands: train, serve, and pytest"
+    echo "No command provided. Available commands: train, serve, launch, and pytest"
 fi
