60e2eee7555e172087e0e5c3e70c47eef79c9d83
dennism-tulcolabs <dennis.michalopoulos@tulcolabs.com>
2025-04-04 20:27:14 -0500

    Delete commented code

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@
 COMMAND="$1"
 
 docker volume rm usf-model-api-root
-#docker build --no-cache -t usf-model-api:latest .
-#docker build --platform linux/amd64 --no-cache -t usf-model-api:latest .
 
 # Build arm64 if specified, or by default
 if [[ arm == $COMMAND ]] || [[ -z $COMMAND ]]; then
